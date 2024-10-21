ebaf16f34302fde0d3075a966e035c06b65f1b67
Chef <cheftech@patraldo.com>
2024-10-21 15:33:56 -0600

    -

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,11 @@
 <script>
+        import { onMount } from "svelte";
+
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hola {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
